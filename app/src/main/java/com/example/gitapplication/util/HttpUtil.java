50d2f561cc7b626982ac54705ef4ad032c0b24d7
YXL <97509746@qq.com>
2020-12-04 08:17:55 +0800

    加入显示天气信息

--- app/src/main/java/com/example/gitapplication/util/HttpUtil.java
+++ app/src/main/java/com/example/gitapplication/util/HttpUtil.java
@@ -1,13 +1,16 @@
 package com.example.gitapplication.util;
 
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 public class HttpUtil {
-    public static void sendOkHttpRequest(String address, Callback callback){
-        OkHttpClient client=new OkHttpClient();
-        Request request=new Request.Builder().url(address).build();
+    private static final String TAG = "HttpUtil";
+
+    //    发起请求只需要调用sendOkHttpRequest()方法，
+//          传入请求地址，并注册一个回调用来处理服务器响应
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
     }
 }
